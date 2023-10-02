7913a20ca03457b29918459863b0149aec5b0401
olododo omotomiwa <omotomiwar@gmail.com>
2023-10-02 10:52:09 +0100

    add this login function and signup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from dotenv import load_dotenv
 import sqlite3
 import requests
@@ -23,6 +23,48 @@ CREATE TABLE IF NOT EXISTS users(
 '''
 
 cursor.execute(query)
+
+
+app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == "PSOT":
+        email = request.form['']
+        password = request.form['']
+
+        query = ("SELECT * FORM users WHERE email = '"+email+"' AND  password = '"+password+"' ")
+        cursor.execute(query)
+
+        result = cursor.fetchall()
+
+        if len(result) == 0:
+            print("unknown users")
+        else:
+            return redirect("/home", code = 302)
+        
+    return render_template('login.html')
+                 
+
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form['']
+        email = request.form['']
+        age = request.form['']
+        password = request.form['']
+
+        cursor.execute("INSERT INTO users VALUES (?,?,?,?,?)",userid , username, email, age, password)
+        connection.commit()
+
+        return login()
+    return render_template("signup.html")
+
+
+@app.route('/', methods= ['GET', 'POST'])
+@app.route('/home', methods= ['GET', 'POST'])
+def home():
+    pass
+
 
 def api_call():
     pass
